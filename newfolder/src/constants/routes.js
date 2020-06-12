bd5a6bc109f51cfb35d9000dca6cbae9f31844f9
nishthapant <48161605+nishthapant@users.noreply.github.com>
2020-06-12 16:41:26 -0600

    Add files via upload

---
+++ newfolder/src/constants/routes.js
@@ -0,0 +1,8 @@
+export const HOME = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const SIGN_OUT = '/signout';
+// export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/pw-forget';
+
