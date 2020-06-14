6960cc4d31c6b8f54e2a30b4a43551cde18ca7db
nishthapant <48161605+nishthapant@users.noreply.github.com>
2020-06-13 21:31:58 -0600

    Update App

--- newfolder/src/components/App/index.js
+++ newfolder/src/components/App/index.js
@@ -5,9 +5,14 @@ import Navigation from '../Navigation';
 import SignUp from '../SignUp';
 import SignIn from '../SignIn';
 import Home from '../Home';
-import PasswordForget from '../PasswordForget';
+import Instructors from '../Instructors';
+import Subjects from '../Subjects';
+import About from '../About';
+import Contact from '../Contact';
+import Blogs from '../Blogs';
+// import PasswordForget from '../PasswordForget';
 // import PasswordChange from '../PasswordChange';
-import Account from '../Account';
+// import Account from '../Account';
 
 import * as ROUTES from '../../constants/routes';
 
@@ -20,9 +25,13 @@ const App = ()=>(
             <Route exact path={ROUTES.HOME} component={Home}/>
             <Route path={ROUTES.SIGN_UP} component={SignUp}/>
             <Route path={ROUTES.SIGN_IN} component={SignIn}/>
-            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget}/>
+            <Route path={ROUTES.INSTRUCTORS} component={Instructors}/>
+            <Route path={ROUTES.SUBJECTS} component={Subjects}/>
+            <Route path={ROUTES.ABOUT} component={About}/>
+            <Route path={ROUTES.CONTACT} component={Contact}/>
+            <Route path={ROUTES.BLOGS} component={Blogs}/>
             {/* <Route path={ROUTES.PASSWORD_CHANGE} component={PasswordChange}/> */}
-            <Route path={ROUTES.ACCOUNT} component={Account}/>
+            {/* <Route path={ROUTES.ACCOUNT} component={Account}/> */}
 
         </div>
     </Router>
