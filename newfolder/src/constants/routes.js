7afeb2831cad9d253df06a953c83cb46655d5650
nishthapant <48161605+nishthapant@users.noreply.github.com>
2020-06-13 21:23:59 -0600

    Update routes.js

--- newfolder/src/constants/routes.js
+++ newfolder/src/constants/routes.js
@@ -1,8 +1,16 @@
 export const HOME = '/';
+
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const SIGN_OUT = '/signout';
-// export const HOME = '/home';
+
+export const INSTRUCTORS = '/instructors'
+export const SUBJECTS = '/subjects';
+export const ABOUT = '/about';
+export const CONTACT = '/contact';
+export const BLOGS = '/blogs';
+
+
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
 
