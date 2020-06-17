a0a93443b7d28c5744ed809be1f6444a42b67da9
nishthapant <48161605+nishthapant@users.noreply.github.com>
2020-06-17 13:12:52 -0600

    create navigation bar

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes'; 
+
+const Navigation = ()=>(
+    <div>
+        <ul>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.INSTRUCTORS}>Instructors</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SUBJECTS}>Subjects</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.BLOGS}>Blogs</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ABOUT}>About</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.CONTACT}>Contact Us</Link>
+            </li>
+        </ul>
+    </div>
+);
+
+export default Navigation;
